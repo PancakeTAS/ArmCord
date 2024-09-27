a850bdfc2cebd80be9b8059094888c006ec4f69d
PancakeTAS <pancake@mgnet.work>
2024-09-27 21:25:58 +0200

    fix: tests

--- src/shelter/screenshareQualityFix/index.js
+++ src/shelter/screenshareQualityFix/index.js
@@ -15,12 +15,58 @@ function onStreamQualityChange() {
     if (streamConnection) {
         streamConnection.videoStreamParameters[0].maxFrameRate = store.fps;
         streamConnection.videoStreamParameters[0].maxResolution.height = store.resolution;
-        streamConnection.videoStreamParameters[0].maxResolution.width = Math.round(store.resolution * (16 / 9));
-        streamConnection.videoQualityManager.goliveMaxQuality.bitrateMin = 500000;
-        streamConnection.videoQualityManager.goliveMaxQuality.bitrateMax = 8000000;
-        streamConnection.videoQualityManager.goliveMaxQuality.bitrateTarget = 600000;
+        streamConnection.videoStreamParameters[0].maxResolution.width = store.resolution * (16 / 9);
+        streamConnection.videoQualityManager.goliveMaxQuality.bitrateMin = 20000000;
+        streamConnection.videoQualityManager.goliveMaxQuality.bitrateMax = 20000000;
+        streamConnection.videoQualityManager.goliveMaxQuality.bitrateTarget = 20000000;
         log(`Patched current user stream with resolution: ${store.resolution} and fps: ${store.fps}`);
     }
+
+    shelter.patcher.instead(
+        "applyQualityConstraints",
+        streamConnection,
+        (args, originalFunction) => {
+            let e = args[0];
+            e.encodingVideoWidth = store.resolution;
+            e.encodingVideoHeight = store.resolution * (16 / 9);
+            e.encodingVideoFrameRate = store.fps;
+            e.captureVideoFrameRate = store.fps;
+            e.encodingVideoBitRate = 20000000;
+            e.encodingVideoMinBitRate = 20000000;
+            e.remoteSinkWantsFrameRate = store.fps;
+            return {
+                quality: {
+                    bitrateMin: 20000000,
+                    bitrateMax: 20000000,
+                    bitrateTarget: 20000000,
+                    encode: {
+                        pixelCount: e.encodingVideoWidth * e.encodingVideoHeight,
+                        framerate: e.encodingVideoFrameRate
+                    }
+                },
+                constraints: e
+            }
+        }
+    );
+
+    shelter.patcher.instead(
+        "pickProperties",
+        streamConnection,
+        (args, originalFunction) => {
+            return {
+                "remoteSinkWantsFrameRate": store.fps,
+                "remoteSinkWantsPixelCount": store.resolution * (store.resolution * (16 / 9)),
+                "encodingVideoMinBitRate": 20000000,
+                "encodingVideoMaxBitRate": 20000000,
+                "encodingVideoBitRate": 20000000,
+                "encodingVideoWidth": store.resolution,
+                "encodingVideoHeight": store.resolution * (16 / 9),
+                "encodingVideoFrameRate": store.fps,
+                "captureVideoFrameRate": store.fps,
+                "streamParameters": args[0].streamParameters
+            }
+        }
+    );
 }
 export function onLoad() {
     dispatcher.subscribe("MEDIA_ENGINE_VIDEO_SOURCE_QUALITY_CHANGED", onStreamQualityChange);
